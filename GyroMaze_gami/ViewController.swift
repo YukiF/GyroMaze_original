f4e1f770e10774fe97d571969de6d180d637197f
Yuki Futagami <drizzle_cold_0904@yahoo.co.jp>
2015-11-26 15:35:24 +0900

    Initial Commit

---
+++ GyroMaze_gami/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GyroMaze_gami
+//
+//  Created by Yuki.F on 2015/11/26.
+//  Copyright © 2015年 Yuki Futagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
